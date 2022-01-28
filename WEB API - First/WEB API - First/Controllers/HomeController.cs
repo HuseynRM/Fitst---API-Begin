6fcd8d2e8e18605e1f4c5fe079ef7e83028fd584
huseyn memmedov <huseyn.memmedov.66@outlook.com>
2022-01-29 03:13:16 +0400

    Mehtods added and run postman

---
+++ WEB API - First/WEB API - First/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WEB_API___First.Controllers
+{
+    [ApiController]
+    [Route("api/[Controller]")]
+    public class HomeController : ControllerBase
+    {
+
+        //eyni adda method yazanda artiq bu methodsignature uygun deyil deye biz hansisa mmethodun adini deyishmeliyik !!!
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("First Get");
+        }
+        [HttpGet]
+        [Route("second")]
+        public IActionResult Get2(int id)
+        {
+            return Ok("Secont Get");
+        }
+        [HttpPost]
+        public IActionResult Post()
+        {
+            return Ok("Hello Post Method");
+        }
+        [HttpPost]
+        [Route("second")]
+        public IActionResult Post2()
+        {
+            return Ok("Hello Post2 Method");
+        }
+        //Crud daki updatedi ve biz burda mutleq obyekt yollamaliyiq
+        [HttpPut]
+        public IActionResult Put()
+        {
+            return Ok("Update ishledi");
+        }
+        [HttpDelete]
+        public IActionResult Delete()
+        {
+            return NoContent();
+        }
+    }
+}
