642a787009be679ea43cb7d3140b5e45d991229e
huseyn memmedov <huseyn.memmedov.66@outlook.com>
2022-01-29 19:15:11 +0400

    SeedDate

--- WEB API - First/WEB API - First/Data/AppDbContext.cs
+++ WEB API - First/WEB API - First/Data/AppDbContext.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WEB_API___First.Models;
 
 namespace WEB_API___First.Data
 {
@@ -11,6 +12,34 @@ namespace WEB_API___First.Data
         public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
         {
 
+        }
+        public virtual  DbSet<Student> Students { get; set; }
+
+        //seed data  
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Student>().HasData(
+                new Student
+                {
+                    Id = 1 ,
+                    Name = "Huseyn" ,
+                    Surname= "Mammadov", 
+                    CreatedAt=DateTime.UtcNow.AddHours(+4), 
+                    CreatedBy="System", 
+                    Isdeleted = false
+                },
+                 new Student
+                 {
+                     Id = 2,
+                     Name = "Ali",
+                     Surname = "Aliyev",
+                     CreatedAt = DateTime.UtcNow.AddHours(+4),
+                     CreatedBy = "System",
+                     Isdeleted = false
+                 }
+            );
         }
     }
 }
---
+++ WEB API - First/WEB API - First/Migrations/20220129141719_AddedStudentTable_1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WEB_API___First.Migrations
+{
+    public partial class AddedStudentTable_1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Students",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    Surname = table.Column<string>(nullable: true),
+                    Isdeleted = table.Column<bool>(nullable: false),
+                    CreatedAt = table.Column<DateTime>(nullable: true),
+                    CreatedBy = table.Column<string>(nullable: true),
+                    DeletedAt = table.Column<DateTime>(nullable: true),
+                    DeletedBy = table.Column<string>(nullable: true),
+                    UpdateAt = table.Column<DateTime>(nullable: true),
+                    UpdateBy = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Students", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Students",
+                columns: new[] { "Id", "CreatedAt", "CreatedBy", "DeletedAt", "DeletedBy", "Isdeleted", "Name", "Surname", "UpdateAt", "UpdateBy" },
+                values: new object[] { 1, new DateTime(2022, 1, 29, 18, 17, 18, 961, DateTimeKind.Utc).AddTicks(6401), "System", null, null, false, "Huseyn", "Mammadov", null, null });
+
+            migrationBuilder.InsertData(
+                table: "Students",
+                columns: new[] { "Id", "CreatedAt", "CreatedBy", "DeletedAt", "DeletedBy", "Isdeleted", "Name", "Surname", "UpdateAt", "UpdateBy" },
+                values: new object[] { 2, new DateTime(2022, 1, 29, 18, 17, 18, 961, DateTimeKind.Utc).AddTicks(8784), "System", null, null, false, "Ali", "Aliyev", null, null });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Students");
+        }
+    }
+}
